7ef8a47b3e2a85194eff8ba0b0e4e19479cc24b6
Arihant Garg <arihantgarg.ishu@gmail.com>
2023-02-16 18:18:38 +0530

    Corrected error

--- Algorithms/Dynamic Programming/PalindromePartitioning.cpp
+++ Algorithms/Dynamic Programming/PalindromePartitioning.cpp
@@ -2,44 +2,51 @@
 using namespace std;
 
 
-int table[1000][1000];
-
-
-bool checkPalindrome(string s,int start,int end)
+int MinPalindromePartition(string s)
 {
-    while(start<end)
-    {
-        if(s[start]!=s[end])
-            return 0;
+    int n=s.size();
+    int table[n][n];
+    int partition[n];
 
-        start++;
-        end--;
+    for(int i=0;i<n;i++)
+        table[i][i]=1;
+
+    for(int size=2;size<=n;size++)
+    {
+        for(int i=0;i<=n-size;i++)
+        {
+            int j=i+size-1;
+
+            if(s[i]==s[j] && size==2)
+            {
+                table[i][j]=1;
+            }
+            else if(s[i]==s[j] && table[i+1][j-1]==1)
+            {
+                table[i][j]=1;
+            }
+            else
+                table[i][j]=0;
+        }
     }
 
-    return 1;
-}
 
-
-int MinPalindromePartition(string s,int start,int end)
-{
-    if(start>=end || checkPalindrome(s,start,end))
-        return 0;
-
-    if(table[start][end]!=-1)
-        return table[start][end];
-
-    int ans=end-start,count=0;
-
-    for(int i=start;i<end;i++)
+    for(int i=0;i<n;i++)
     {
-        count=MinPalindromePartition(s,start,i)+MinPalindromePartition(s,i+1,end)+1;
-
-        ans=min(ans,count);
+        if(table[0][i]==1)
+            partition[i]=0;
+        else
+        {
+            partition[i]=1e9;
+            for(int j=0;j<i;j++)
+            {
+                if(table[j+1][i]==1)
+                    partition[i]=min(partition[i],1+partition[j]);
+            }
+        }
     }
 
-    table[start][end]=ans;
-
-    return ans;
+    return partition[n-1];
 }
 
 
@@ -48,13 +55,7 @@ int main()
     string s;
     cin>>s;
 
-    int n=s.size();
-
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            table[i][j]=-1;
-
-    cout<<"Minimum number of partitions needed is : "<<MinPalindromePartition(s,0,n-1);
+    cout<<"Minimum number of partitions needed is : "<<MinPalindromePartition(s);
 
     return 0;
 }
